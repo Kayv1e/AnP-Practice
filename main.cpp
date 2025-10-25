d093d5c3f0cd8cd68c805b29821670ce87f45874
Kayv1e <krutojpacanivan@gmail.com>
2025-10-25 22:33:14 +0300

    Added endless input until EOF is present

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 int main()
 {
   using u_t = unsigned;
+  size_t count = 0;
   u_t a = 0;
-  std::cin >> a;
+
+  while (std::cin >> a) {
+    count++;
+  }
 
   if (std::cin.eof()) {
-    std::cout << 0;
+    std::cout << count;
     std::cout << "\n";
   } else if (std::cin.fail()) {
     std::cerr << "Input error\n";
