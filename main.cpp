862512c4202e36ab3b1d732bef72b648ae524d59
Kayv1e <krutojpacanivan@gmail.com>
2025-10-25 22:29:13 +0300

    Added input correction check

--- main.cpp
+++ main.cpp
@@ -5,4 +5,12 @@ int main()
   using u_t = unsigned;
   u_t a = 0;
   std::cin >> a;
+
+  if (std::cin.eof()) {
+    std::cout << 0;
+    std::cout << "\n";
+  } else if (std::cin.fail()) {
+    std::cerr << "Input error\n";
+    return 1;
+  }
 }
